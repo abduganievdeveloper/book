5d8b83f7908e84bb014324651bb3c19e817e0603
AbdumalikFront <iwillstudyingermany@gmail.com>
2019-05-23 08:14:49 +0500

    image addresses modified

---
+++ precache-manifest.fca893b15161fd770ae0ca5a7d2b2671.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ee50ceb26310c12a9536fb0708f9cff",
+    "url": "/book/index.html"
+  },
+  {
+    "revision": "230439a11eaa44cf1f95",
+    "url": "/book/static/css/main.661949f2.chunk.css"
+  },
+  {
+    "revision": "13a251f4032dc8f00fbd",
+    "url": "/book/static/js/2.2fe2e457.chunk.js"
+  },
+  {
+    "revision": "230439a11eaa44cf1f95",
+    "url": "/book/static/js/main.4a7df97c.chunk.js"
+  },
+  {
+    "revision": "32ef9d0f082932ab0295",
+    "url": "/book/static/js/runtime~main.938e06c9.js"
+  }
+]);
